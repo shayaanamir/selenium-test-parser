477e96cff97696c1378afe2b1c393253cd757c6f
Shayaan Amir <67624423+PlatinumBeing@users.noreply.github.com>
2025-07-10 14:09:18 +0530

    removed redundant code

--- src/main/java/com/testparser/extractors/PageObjectExtractor.java
+++ src/main/java/com/testparser/extractors/PageObjectExtractor.java
@@ -67,7 +67,6 @@ public class PageObjectExtractor {
      * Determine if a Java file is likely a page object based on location and content
      */
     private static boolean isPageObjectFile(File file) {
-        String fileName = file.getName();
         String parentDirName = file.getParentFile().getName().toLowerCase();
         
         // Check if file is in page/pages folder
